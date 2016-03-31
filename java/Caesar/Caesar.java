141fec6365656e072801e57a991208fb1bbb6208
the_octagon <andrew.f.king+github@gmail.com>
2016-03-31 14:37:35 -0400

    add comments and remove depreciated code

--- java/Caesar/Caesar.java
+++ java/Caesar/Caesar.java
@@ -18,6 +18,7 @@ public class Caesar {
 			System.out.println("1. Encrypt");
 			System.out.println("2. Decrypt");
 			System.out.println("3. Exit");
+			//input into string with try catch leading into validation loop, ensuring 1 2 or 3 put into options case switch
 			optionString = keyboard.nextLine();
 			try {
 				option = Integer.parseInt(optionString);
@@ -47,6 +48,8 @@ public class Caesar {
 		}
 	}
 
+
+	//steps to run through for encryption route
 	public static void encryptMessage() {
 		int shift = getShift();
 		String message = getMessage();
@@ -54,12 +57,15 @@ public class Caesar {
 		printOut(encrypted);
 	}
 
+	//steps to run through for decryption route
 	public static void decryptMessage() {
 		int shift = getShift();
 		String message = getMessage();
 		String decrypted = decrypt(message,shift);
 		printOut(decrypted);
 	}
+
+	//take user input for shift value, feed through try catch to check value is numeric and doesn't violate rules
 	public static int getShift(){
 		System.out.println("Enter the shift value");
 		int shift = 0;
@@ -69,8 +75,8 @@ public class Caesar {
 		} catch (InputMismatchException e) {
 		}catch (NumberFormatException f){
 		}
-		while (shift==31||shift==0){
-			System.out.println("Please enter a numerical value.");
+		while (shift % 41 == 0){
+			System.out.println("Please enter a whole number that is not a multiple of 41.");
 			shiftString = keyboard.nextLine();
 			try {
 				shift = Integer.parseInt(shiftString);
@@ -81,41 +87,18 @@ public class Caesar {
 		return shift;
 	}
 
+	//take user input with readPassword function to hide CLI input and save to char array, save to string for easy toLower
 	public static String getMessage() {
 		Console console = System.console();
 		System.out.println("Enter your message. (Text will not appear)");
 		char[] messageArray = console.readPassword();
-
-
 		String message = new String(messageArray);
 		System.out.println();
 		message = message.toLowerCase();
 		return message;
 	}
 
-	//version 1 of encrypted, very limited
-/*	public static String encrypt(String message,int shift){
-		int length = message.length();
-		char[] messageArray = message.toCharArray();
-		char[] encryptedArray = new char[length];
-		for (int index = 0;index < length; index++) {
-			encryptedArray[index] = (char)(messageArray[index] + shift);
-		}
-		String encrypted = new String(encryptedArray);
-		return encrypted;
-	}*/
-
-/*	public static String decrypt(String message,int shift) {
-		int length = message.length();
-		char[] messageArray = message.toCharArray();
-		char[] decryptedArray = new char[length];
-		for (int index = 0;index < length; index++) {
-			decryptedArray[index] = (char)(messageArray[index]-shift);
-		}
-		String decrypted = new String(decryptedArray);
-		return decrypted;
-	}*/
-
+	//dump message to stdout
 	public static void printOut(String message) {
 		System.out.println("Your secret message is:");
 		System.out.println("\""+message+"\"");
@@ -123,33 +106,43 @@ public class Caesar {
 		System.out.println();
 	}
 
+	//v2 of encrypt function that removes most limitations on functionality.
 	public static String encrypt(String message, int shift) {
-		char[] key =  {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','.',',','?','/','\''};
+		char[] key =  {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','.',',','?','/','\'','1','2','3','4','5','6','7','8','9','0'};
 		int length = message.length();
 		char[] messageArray = message.toCharArray();
 		char[] encryptedArray = new char[length];
 		int keyIndex = 0;
 		int shiftIndex = 0;
+		//run once per char of word
 		for (int index = 0;index < length;index++) {
+			//search for char of message in key array
 			while (messageArray[index]!=key[keyIndex]){
 				keyIndex++;
 			}
+			//get index of shifted letter
 			shiftIndex = keyIndex + shift;
-			while (shiftIndex>31){
-				shiftIndex = shiftIndex - 31;
+			//normalize for too high values
+			while (shiftIndex>41){
+				shiftIndex = shiftIndex - 41;
 			}
+			//normalize for too low values, account for 0 as allowed value
 			while (shiftIndex<0){
-				shiftIndex = shiftIndex + 32;
+				shiftIndex = shiftIndex + 42;
 			}
+			//set letter in encrypted message as normalized shifted letter
 			encryptedArray[index] = key[shiftIndex];
+			//reset keyIndex
 			keyIndex = 0;
 		}
+		//change to string
 		String encrypted = new String(encryptedArray);
 		return encrypted;
 	}
 
+	//v2 of decrypt function... see above for step by step. only difference is subtract instead of add shiftIndex
         public static String decrypt(String message, int shift) {
-                char[] key =  {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','.',',','?','/','\''};
+                char[] key =  {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','.',',','?','/','\'','1','2','3','4','5','6','7','8','9','0'};
                 int length = message.length();
                 char[] messageArray = message.toCharArray();
                 char[] decryptedArray = new char[length];
@@ -161,10 +154,10 @@ public class Caesar {
                         }
                         shiftIndex = keyIndex - shift;
                         while (shiftIndex<0) {
-                                shiftIndex = shiftIndex + 31;
+                                shiftIndex = shiftIndex + 41;
                         }
-			while (shiftIndex>31) {
-				shiftIndex = shiftIndex - 32;
+			while (shiftIndex>41) {
+				shiftIndex = shiftIndex - 42;
 			}
                         decryptedArray[index] = key[shiftIndex];
                         keyIndex = 0;
@@ -174,4 +167,3 @@ public class Caesar {
         }
 
 }
-
