b6f6e39267b4d2a26a9be025c9b9d05a6f30ea7d
the_octagon <andrew.f.king+github@gmail.com>
2016-04-01 20:58:19 -0400

    SlotMachine working- needs fancy printout

--- java/SlotMachine/SlotMachine.java
+++ java/SlotMachine/SlotMachine.java
@@ -58,14 +58,126 @@ public class SlotMachine {
 	}
 
 	public static void pullLever() {
-		//gen random 1
-		//gen random 2
-		//gen random 3
-		//gen random 4
-		//how many things are on a slot machine???
+		int[] wheel = new int[3];
+		String[] wheelHuman = new String[3];
+		int combo = 0;
+		boolean threeWay = false;
 
-		//if combo, then win?
+		for (int index = 0; index < 3; index++) {
+			wheel[index] = rollWheel();
+			wheelHuman[index] = getWheelHuman(wheel[index]);
+		}
+
+		if (wheel[0] == wheel[1] && wheel[0] == wheel[2]) {
+			threeWay = true;
+		}
+
+		if (threeWay == true) {
+			if (wheel[0] == 1) {
+				combo = 1;
+			} else if (wheel[0] == 2) {
+				combo = 2;
+			} else if (wheel[0] == 3) {
+				combo = 3;
+			} else if (wheel[0] == 4) {
+				combo = 4;
+			} else if (wheel[0] == 5) {
+				combo = 5;
+			} else if (wheel[0] == 6) {
+				combo = 6;
+			}
+		} else if ((wheel[0] == 3|| wheel[0] == 4 || wheel[0] == 5) && (wheel[1] == 3|| wheel[1] == 4 || wheel[1] == 5) && (wheel[2] == 3|| wheel[2] == 4 || wheel[2] == 5)) {
+			combo = 7;
+		} else if (wheel[0] == 6 || wheel[1] == 6 || wheel[2] == 6) {
+			combo = 8;
+		} else {
+			combo = 9;
+		}
+
+//		results(combo);
+
+		System.out.println(wheel[0] + " " + wheel[1] + " " + wheel[2]);
+		System.out.println(wheelHuman[0] + " " + wheelHuman[1] + " " + wheelHuman[2]);
+		System.out.println(combo);
+
+
 	}
 
+	public static int rollWheel() {
 
+		//random between 1 and 7
+		return 1 + (int)(Math.random() * ((6 - 1) + 1));
+
+	}
+
+	public static String getWheelHuman(int wheel) {
+		String wheelHuman = new String();
+
+		switch (wheel) {
+			case 1:
+				wheelHuman = "Diamonds";
+				break;
+
+			case 2:
+				wheelHuman = "Seven";
+				break;
+
+			case 3:
+				wheelHuman = "Bar x3";
+				break;
+
+			case 4:
+				wheelHuman = "Bar x2";
+				break;
+
+			case 5:
+				wheelHuman = "Bar";
+				break;
+
+			case 6:
+				wheelHuman = "Cherry";
+				break;
+		}
+		return wheelHuman;
+
+	}
+/*
+	public static void results(int combo, int[] wheel) {
+		switch(combo) {
+			case 1:
+				
+				break;
+
+			case 2:
+				
+				break;
+
+			case 3:
+				
+				break;
+
+			case 4:
+				
+				break;
+
+			case 5:
+				
+				break;
+
+			case 6:
+				
+				break;
+
+			case 7:
+				
+				break;
+
+			case 8:
+				
+				break;
+		}
+
+	}
+*/
 }
+
